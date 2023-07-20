6e0e1d2104c7c6160e7643db82c01d829626bfa5
Goooler <wangzongler@gmail.com>
2023-07-20 10:54:39 +0800

    Enable build scan publishing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,16 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("com.gradle.enterprise") version "3.14"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
+
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
